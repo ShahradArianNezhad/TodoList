3892c25d14f14db6a207da1dc5945ff6a804d5d1
shahrad <shahradArianNezhad@gmail.com>
2025-07-26 20:15:55 +0330

    added routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,13 @@
+import { BrowserRouter,Routes,Route } from "react-router-dom"
+import Home from "./pages/Home"
 
 const App = () =>{
   return(
-    <>
-      <h1 className='text-3xl text-amber-300'>hello</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
