6b03b0ba972de0a376bef32898835c1a1e584d6e
shahrad <shahradArianNezhad@gmail.com>
2025-07-31 19:50:04 +0330

    added login and register frontend files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter,Routes,Route } from "react-router-dom"
 import Home from "./pages/Home/Home"
 import { AuthProvider } from "./providers/authContext"
+import Login from "./pages/users/login/Login"
+import Register from "./pages/users/register/Register"
 
 const App = () =>{
   return(
@@ -8,6 +10,8 @@ const App = () =>{
       <AuthProvider>
         <Routes>
           <Route path="/" element={<Home/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/register" element={<Register/>}/>
         </Routes>
       </AuthProvider>
     </BrowserRouter>
---
+++ frontend/src/pages/users/register/Register.tsx
@@ -0,0 +1,24 @@
+import Navbar from "../../../components/Navbar"
+import { Link } from 'react-router-dom';
+
+
+
+const Register = () => {
+  return (<>
+    <Navbar/>
+    <div className="w-screen h-screen bg-[#ecebc7] flex justify-center items-center flex-col">
+        <h1 className=" text-5xl pb-3 font-bold text-gray-800">Register</h1>
+        <div className="w-140 h-120 bg-slate-800 rounded-4xl flex justify-center items-center border-4 border-slate-900 shadow-sm shadow-black">
+            <form action="http://localhost:8000/user/register" method="post" className="flex justify-around items-center flex-col w-full h-[60%]">
+                <input type="text" name="username" id="username" placeholder="Username" className="bg-gray-900 w-70 px-3 py-2 outline-0 rounded-md text-gray-400 shadow-black shadow-sm focus:shadow-md transition-all duration-100 "/>
+                <input type="text" name="password" id="password" placeholder="Password" className="bg-gray-900 w-70 px-3 py-2 outline-0 rounded-md text-gray-400 shadow-black shadow-sm focus:shadow-md transition-all duration-100"/>
+                <button type="submit" className="font-medium tracking-wide bg-gray-900 w-70 px-3 py-2 outline-0 rounded-md text-gray-400 mt-5 mb-4 cursor-pointer shadow-black shadow-sm hover:shadow-md transition-all duration-100">Register</button>
+                <span className="text-sm text-gray-400 tracking-wide font-medium">Already have an account? <Link to="/login" className="text-blue-300 hover:text-blue-400 transition-all duration-200 underline-offset-3 underline cursor-pointer">Login</Link> </span>
+            </form>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Register
