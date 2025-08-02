e7f5b3175d4b5aff296b3c32da90400ef97b9211
shahrad <shahradArianNezhad@gmail.com>
2025-08-02 20:54:43 +0330

    removed non working function

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,18 +1,11 @@
-import { Navigate, useNavigate } from "react-router-dom"
+
 import Navbar from "../../components/Navbar"
-import { useContext, useEffect } from "react"
-import { AuthContext } from "../../providers/authContext"
+
+
+
 
 
 const Home = () => {
-  const nav = useNavigate()
-  const x= useContext(AuthContext)
-
-  useEffect(()=>{
-  if(!x?.auth){
-    nav("/login")
-  }
-  })
 
 
 
