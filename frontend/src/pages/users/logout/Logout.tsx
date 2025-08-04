3cb4439bd776ce6954b914751bec9dc490bb11cf
shahrad <shahradArianNezhad@gmail.com>
2025-08-04 18:52:40 +0330

    fixed a issue with logout comp

--- frontend/src/pages/users/logout/Logout.tsx
+++ frontend/src/pages/users/logout/Logout.tsx
@@ -33,7 +33,7 @@ const Logout = () => {
         performLogout()
     })
 
-
+    return<></>
 
 
 }
--- frontend/src/providers/authContext.tsx
+++ frontend/src/providers/authContext.tsx
@@ -17,21 +17,6 @@ export interface AuthInterface{
 
 export const AuthContext = createContext<AuthInterface|undefined>(undefined);
 
-// const register = async()=>{
-
-//     await fetch("http://localhost:8000/user/login",{
-//         method:"POST",
-//         headers:{
-//             'Content-Type':'application/json'
-//         },
-//         body:JSON.stringify({
-//             username:"allahhhhh3",
-//             password:"129121271212"
-//         }),
-//         credentials:'include'
-//     })
-// }
-
 
 
 
@@ -46,26 +31,26 @@ export const AuthProvider = ({children}:{children:ReactNode})=>{
     const [loading,setIsLoading]= useState(true)
     const [taskList,setTaskList]= useState<recievedTaskArr>()
 
-    const auth = async()=>{
-            const res = await fetch("http://localhost:8000/user/auth",{
-                method:'GET',
-                headers:{
-                    'Content-Type': 'application/json'
-                },
-                credentials:'include'
-            })
-            const result = (await res.json())
-            if (result.status == "authorized"){
-                setIsAuthenticated(true)
-                setUser(result.username)
-                setTaskList(result.tasks)
-                console.log("auth")
-            }else{
-                console.log("unauth")
-            }
-            setIsLoading(false)
-    }
-    auth()
+        const auth = async()=>{
+                const res = await fetch("http://localhost:8000/user/auth",{
+                    method:'GET',
+                    headers:{
+                        'Content-Type': 'application/json'
+                    },
+                    credentials:'include'
+                })
+                const result = (await res.json())
+                if (result.status == "authorized"){
+                    setIsAuthenticated(true)
+                    setUser(result.username)
+                    setTaskList(result.tasks)
+                    console.log("auth")
+                }else{
+                    console.log("unauth")
+                }
+                setIsLoading(false)
+        }
+        auth()
 
 
     return(
